6ada9c6e9a7f8d2808f5c753712bed501f29e472
Andy Sharma <andyshrx@users.noreply.github.com>
2026-07-13 13:36:05 +1000

    oh and a favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,8 @@
 
 <title>{{ $title ?? 'PDFPost' }}</title>
 
+<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
